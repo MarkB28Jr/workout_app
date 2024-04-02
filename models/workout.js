369ed8c8162cbb0be5c5fc06f5eae2d6d87d0f45
MarkB28Jr <mark.brashear265@gmail.com>
2024-04-02 16:51:23 -0500

    Created JSON exercises named by muscleGroup/ working on fetching data and populating data

--- models/workout.js
+++ models/workout.js
@@ -2,11 +2,47 @@ const mongoose = require('mongoose');
 
 const Schema = mongoose.Schema
 
-const workoutSchema = new Schema({
+// const historySchema = new Schema({
+
+// })
+
+const exerciseSchema = new Schema({
+    name: {
+        type: String,
+        required: true
+    },
+    muscleGroup: {
+        type: String,
+        enum: muscleGroup,
+        required: true,
+    },
 
 })
 
-// const historySchema = new Schema({
-// })
+const templateSchema = new Schema({
+    sets: {
+        type: Number,
+    },
+    reps: {
+        type: Number,
+    },
+    weight: {
+        type: Number
+    },
+    exercises: [exerciseSchema]
+})
+
+const workoutSchema = new Schema({
+    musclegroup: {
+        type: String,
+        enum: ['Quadriceps', 'Glutes_Hamstrings', 'Calves', 'Chest', 'Back', 'Shoulders', 'Triceps', 'Biceps', 'Abs'],
+        required: true,
+    },
+
+})
+const historySchema = new Schema({
+
+})
 
 module.exports = mongoose.model('Workout', workoutSchema)
+
